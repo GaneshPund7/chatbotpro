a6bc5c03187332723fb150ee8b6448a04859b029
Ganeshpund7 <ganeshpund0000@gmail.com>
2024-12-15 13:30:50 +0530

    changes in path

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       plugins: [tailwindcss()]
     }
   },
-  base: '/metronic/tailwind/react',
+  base: 'chatbot',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
